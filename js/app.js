fbd1f5c17013d8b81643859a91f9cb4bf97c2941
Terri-Chong <terrichong@gmail.com>
2018-06-25 21:59:51 -0700

    added enemy's speed and implement enemy's update method

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function(y) {
+var Enemy = function(x, y, speed = 100) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
+    this.x = x;
     this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -16,6 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    if (this.x > 505) {
+      this.x = 0;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -48,7 +53,7 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = [new Enemy(53), new Enemy(136), new Enemy(219)];
+let allEnemies = [new Enemy(0, 53, 100), new Enemy(101, 136, 150), new Enemy(202, 219, 200)];
 let player = new Player();
 
 // This listens for key presses and sends the keys to your
