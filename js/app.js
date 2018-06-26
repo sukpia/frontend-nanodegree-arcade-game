b29c03eb0b928e413ff6dc92a524c967e50b4d46
Terri-Chong <terrichong@gmail.com>
2018-06-25 21:10:40 -0700

    created and instantiated enemy and player objects

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -24,13 +26,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.x = 202;
+  this.y = 385;
+};
+
+// Update the player's position, required method for game
+Player.prototype.update = function() {
+
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Receive user input, required method for game
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+let allEnemies = [new Enemy(53), new Enemy(136), new Enemy(219)];
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
