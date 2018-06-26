b16fd6dfe80dbbe2eb1e2646b7b9a7f3f560a585
Terri-Chong <terrichong@gmail.com>
2018-06-25 23:08:04 -0700

    implement player's handleInput method

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ Enemy.prototype.render = function() {
 var Player = function() {
   this.sprite = 'images/char-boy.png';
   this.x = 202;
-  this.y = 385;
+  this.y = 400;
 };
 
 // Update the player's position, required method for game
@@ -48,12 +48,52 @@ Player.prototype.render = function() {
 };
 
 // Receive user input, required method for game
+// Parameter: allowedKeys, the key user pressed
+// Output: move the player according to the key pressed
+//  1. left arrow - move player to the left
+//  2. right arrow - move player to the right
+//  3. up arrow - move player up
+//  4. down arrow - move player down
+Player.prototype.handleInput = function(keypressed) {
+  switch (keypressed) {
+    case 'left':
+      if (this.x <= 0) {
+        this.x = 0;
+      } else {
+        this.x -= 101;
+      }
+      break;
+    case 'right':
 
+      if (this.x >= 404) {
+        this.x = 404;
+      } else {
+        this.x += 101;
+      }
+      break;
+    case 'up':
+      this.y -= 83;
+      // reach the water, win the game
+      if (this.y <= -15) {
+        // reset player to initial position
+        this.y = 400;
+        this.x = 202;
+      }
+      break;
+    case 'down':
+      if (this.y >= 400) {
+        this.y = 400;
+      } else {
+        this.y += 83;
+      }
+      break;
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = [new Enemy(0, 53, 100), new Enemy(101, 136, 150), new Enemy(202, 219, 200)];
+let allEnemies = [new Enemy(0, 63, 100), new Enemy(101, 146, 150), new Enemy(202, 229, 200)];
 let player = new Player();
 
 // This listens for key presses and sends the keys to your
